e8214407b314b32dc843fe411f5f61a00e49907f
Gaurav Gehlot <75985096+GehlotGaurav@users.noreply.github.com>
2024-10-04 22:45:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = ' YOUR_TELEGRAM_BOT_TOKEN'
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='Hello! I can delete all media and stickers in this group.')
 
 def delete_media(update, context):
     chat_id = update.effective_chat.id
-    for message in context.bot.get_history(chat_id=chat_id):
-        if message.sticker or message.photo or message.video or message.document:
-            context.bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+    offset = 0
+    while True:
+        messages = context.bot.get_history(chat_id=chat_id, offset=offset)
+        if not messages:
+            break
+        for message in messages:
+            if message.sticker or message.photo or message.video or message.document:
+                context.bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+        offset += len(messages)
 
 def main():
     updater = Updater(TOKEN, use_context=True)
