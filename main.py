694e826e5745bb22285d14d39aab2a027c5e7c7d
Gaurav Gehlot <75985096+GehlotGaurav@users.noreply.github.com>
2024-10-04 22:47:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = '7744724902:AAHZ4xsKvqBqtCoh5tJpE5W-oezL4aXoxp8'
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Hello! I can delete all media and stickers in this group.')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Hello! I can delete all media and stickers in this group.')
 
-def delete_media(update, context):
+async def delete_media(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     offset = 0
     while True:
-        messages = context.bot.get_history(chat_id=chat_id, offset=offset)
+        messages = await context.bot.get_history(chat_id=chat_id, offset=offset)
         if not messages:
             break
         for message in messages:
             if message.sticker or message.photo or message.video or message.document:
-                context.bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+                await context.bot.delete_message(chat_id=chat_id, message_id=message.message_id)
         offset += len(messages)
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
+    application = ApplicationBuilder().token(TOKEN).build()
 
-    dp = updater.dispatcher
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('delete_media', delete_media))
 
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('delete_media', delete_media))
-
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
